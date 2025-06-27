ffc3ae26a29fe9fa0e9bcaf9ed3f6d1876fb5917
Harol <ben14mz@gmail.com>
2025-06-27 18:16:15 -0500

    Primera versión de la API de alerta

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 import pickle
 import pandas as pd
 from datetime import datetime
+import pytz
 
 app = FastAPI()
 
@@ -14,11 +15,9 @@ modelo = data["modelo"]
 le = data["label_encoders"]
 columnas = modelo.feature_names_in_.tolist()
 
-# Traducir código de predicción a tipo de alerta
 def interpretar_alerta(codigo):
     return {1: "leve", 2: "grave", 3: "critica"}.get(codigo, "desconocida")
 
-# Esquema de entrada para la API
 class DatosEntrada(BaseModel):
     n_vehiculo: str
     ruta: str
@@ -28,24 +27,24 @@ class DatosEntrada(BaseModel):
     dia_semana: str
     clima: str
 
-# Endpoint principal
 @app.post("/alerta")
 def predecir_alerta(data: DatosEntrada):
     entrada = data.dict()
 
-    # Codificación de variables categóricas
     entrada["dia_semana"] = le["dia_semana"].transform([entrada["dia_semana"]])[0]
     entrada["clima"] = le["clima"].transform([entrada["clima"]])[0]
     entrada["ruta"] = le["ruta"].transform([entrada["ruta"]])[0]
     entrada["tramo"] = le["tramo"].transform([entrada["tramo"]])[0]
 
-    # Preparar entrada y predecir
     X_nuevo = pd.DataFrame([entrada])[columnas]
     pred = modelo.predict(X_nuevo)[0]
 
+    # Hora local de Perú
+    hora_peru = datetime.now(pytz.timezone("America/Lima")).strftime("%Y-%m-%d %H:%M:%S")
+
     return {
         "n_vehiculo": data.n_vehiculo,
-        "fecha_hora": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "fecha_hora": hora_peru,
         "estado_velocidad": int(pred),
         "tipo_alerta": interpretar_alerta(pred)
     }
